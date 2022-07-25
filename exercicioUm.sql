e26c3c4c7dfd647414326b16d260889b2d75a5ec
Paloma <palomafreiire@hotmail.com>
2022-07-25 16:43:19 -0300

    atualizações ao exercicio um

--- exercicioUm.sql
+++ exercicioUm.sql
@@ -6,33 +6,34 @@ CREATE TABLE tb_rh(
     telefone INT,
     email VARCHAR (255),
     valor_salario INT,
-    area_trabalho VARCHAR (255)
+    cargo VARCHAR (255)
 );
 
-ALTER TABLE tb_rh CHANGE area_trabalho  cargo VARCHAR(255);  -- para alterar o nome da coluna da planilha;
+ -- ALTER TABLE tb_rh CHANGE area_trabalho  cargo VARCHAR(255);  -- para alterar o nome da coluna da planilha;
 ALTER TABLE tb_rh MODIFY COLUMN telefone VARCHAR (255);      -- modificando uma coluna
 
 SELECT * FROM tb_rh;
 
-INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,area_trabalho) VALUES ("Paloma",81122323,"palomafreiire@hotmail.com", 3000, "TI");
-INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,area_trabalho) VALUES ("Camyla",81122323,"camyla123@hotmail.com", 3000, "TI");
-INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,area_trabalho) VALUES ("Antonio",81122323,"antonio345@hotmail.com", 4000, "TI");
-INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,area_trabalho) VALUES ("Michelle",81122323,"michelle78@hotmail.com", 3000, "TI");
-INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,area_trabalho) VALUES ("Deborah",81122323,"Deborahbac@hotmail.com", 2000, "TI");
-INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,area_trabalho) VALUES ("Miguel",81122323,"miguelit@hotmail.com", 1200, "TI");
+INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,cargo) VALUES ("Paloma","81122323","palomafreiire@hotmail.com", 3000, "TI");
+INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,cargo) VALUES ("Camyla",81122323,"camyla123@hotmail.com", 3000, "TI");
+INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,cargo) VALUES ("Antonio",81122323,"antonio345@hotmail.com", 4000, "TI");
+INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,cargo) VALUES ("Michelle",81122323,"michelle78@hotmail.com", 3000, "TI");
+INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,cargo) VALUES ("Deborah",81122323,"Deborahbac@hotmail.com", 2000, "TI");
+INSERT INTO tb_rh (nome_colaborador,telefone,email,valor_salario,cargo) VALUES ("Miguel",81122323,"miguelit@hotmail.com", 1200, "TI");
 
-DELETE FROM tb_rh
-WHERE id = 5;
-
-DELETE FROM tb_rh
-WHERE id = 6;
+-- DELETE FROM tb_rh WHERE id = 5; deletar a linha 5 
 
 UPDATE tb_rh SET valor_salario = 1500
 WHERE id = 4;
 
-UPDATE tb_rh SET  id =5
-WHERE id = 7;
+-- UPDATE tb_rh SET  id =5    WHERE id = 7; para atualizar a linha da tabela
+UPDATE tb_rh SET cargo = "estagiaria" WHERE id = 4;
+UPDATE tb_rh SET cargo = "estagiario" WHERE id=6;
 
-UPDATE tb_rh SET  id =6
-WHERE id = 8;
+DROP TABLE tb_rh;  -- deletar a tabela para depois fazer de novo;
+
+SELECT * FROM tb_rh WHERE valor_salario > 2000;
+SELECT * FROM tb_rh WHERE valor_salario < 2000;
+
+-- SELECT * FROM tb_rh WHERE cargo LIKE "%t%" OR cargo LIKE "t%"; pesquisar o produto dentro da planilha;
 
